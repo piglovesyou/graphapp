f422dc484d0961ce20e1fab527bcccf5865ef0c4
piglovesyou <thepiglovesyou@gmail.com>
2019-07-10 01:47:40 +0900

    It works
    TODO: modify preparing process

--- prepare.sh
+++ prepare.sh
@@ -6,11 +6,12 @@ USER_DIR=`pwd`/examples/basic
 LIB_DIR=`pwd`/packages/uwf
 
 yarn
-yarn lerna bootstrap
+# yarn lerna bootstrap
 
 # Codegen to generate TS files for examples/basic
-(cd $LIB_DIR && yarn && NODE_ENV=develop INIT_CWD=$USER_DIR ./bin/uwf codegen && yarn run prepack)
+# (cd $LIB_DIR && yarn && NODE_ENV=develop INIT_CWD=$USER_DIR ./bin/uwf codegen && yarn run prepack)
 
 # Copy $LIB_DIR as deps of examples/basic by installing all deps
-(cd $USER_DIR && yarn && yarn codegen)
+(cd $USER_DIR && yarn && yarn codegen --develop)
 
+# TODO: Find more smooth way to install uwf from examples/basic
