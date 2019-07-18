64ce22cda40dee4767f87d6cfd601fc89dace3f2
piglovesyou <thepiglovesyou@gmail.com>
2019-07-19 02:14:47 +0900

    Test "build"

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -2,11 +2,15 @@ import _execa from 'execa';
 import path from 'path';
 import waitOn from 'wait-on';
 import { makeDir, cleanDir } from 'uwf/src/tools/lib/fs';
+import i from 'log-symbols';
 import fetch from 'node-fetch';
 import assert from 'assert';
 import terminate from 'terminate';
 
 const timeout = 3 * 60 * 1000;
+
+const success = (...args: string[]) => console.info('\n', i.success, ...args);
+const info = (...args: string[]) => console.info('\n', i.info, ...args);
 
 const execa = (command: string, args: string[], opts?: _execa.Options) =>
   _execa(command, args, { stdout: process.stdout, ...opts });
@@ -21,8 +25,18 @@ const verifyApp = async (port: number) => {
   const text = await fetch(url).then(r => r.text());
   const match = text.match(/<title.*?>(.+?)</);
   if (!match) throw new Error('Title text does not exist');
+
   const [_, actual] = match;
   assert.strictEqual(actual, expected);
+  success(`Verified app of port ${port}`);
+};
+
+const verifyFileExistence = async (files: string[]) => {
+  await waitOn({
+    resources: files,
+    timeout,
+  });
+  success(`Verified files ${files}`);
 };
 
 const startApp = (cwd: string, port: number) =>
@@ -32,17 +46,19 @@ const startApp = (cwd: string, port: number) =>
   });
 
 const kill = async (app: _execa.ExecaChildProcess) => {
+  info(`Terminating app ${app.pid}...`);
   await new Promise((resolve, reject) => {
     terminate(app.pid, (err?: any) => {
       if (err) return reject(err);
       return resolve();
     });
   });
+  success(`App ${app.pid} was terminated`);
 };
 
-describe('Command uwf ', () => {
+describe('uwf ', () => {
   it(
-    '"starts" compiles and starts examples/basic correctly',
+    'examples/basic starts correctly',
     async () => {
       const port = 3010;
       const cwd = path.resolve(__dirname, '../examples/basic');
@@ -54,44 +70,66 @@ describe('Command uwf ', () => {
   );
 
   it(
-    '"init" initialize project from scratch correctly',
+    'Commands "init" "start" and "build" run correctly',
     async () => {
+      const port = 3020;
       const libDir = path.join(__dirname, '../packages/uwf');
       const userDir = path.join(process.env.HOME!, 'tmpUserDir');
       const packedName = './uwf-packed.tgz';
 
+      info(`Preparing "${userDir}" project`);
       await cleanDir(userDir);
       await makeDir(userDir);
       await execa('yarn', ['init', '--yes'], { cwd: userDir });
-
       await execa(
         'yarn',
         ['pack', '--filename', path.join(userDir, packedName)],
         { cwd: libDir },
       );
-
       await execa('yarn', ['--force', 'add', '-D', packedName], {
         cwd: userDir,
       });
 
-      await execa('yarn', ['uwf', 'init'], {
+      info(`Command "init"`);
+      await execa('yarn', ['run', 'uwf', 'init'], {
         cwd: userDir,
       });
+      await verifyFileExistence(
+        [
+          // Verify example copy
+          'routes/about/index.tsx',
+          '.gitignore',
+          // Verify dependency installation
+          'node_modules/node-fetch/package.json',
+        ].map(f => path.join(userDir, f)),
+      );
 
-      const port = 3020;
+      info(`Command "start"`);
       const app = startApp(userDir, port);
-      console.info('verifying..');
       await verifyApp(port);
-      console.info('verified');
-
-      console.info('terminating..');
       await kill(app);
-      console.info('terminated');
+
+      info(`Command "build"`);
+      await execa('yarn', ['run', 'uwf', 'build'], {
+        cwd: userDir,
+      });
+      await cleanDir(path.join(userDir, 'node_modules'));
+      const buildDir = path.join(userDir, 'build');
+      await execa('yarn', [], { cwd: buildDir });
+      info(`Starting built app`);
+      const app2 = execa('node', ['server.js'], {
+        cwd: buildDir,
+        stdout: process.stdout,
+        detached: true,
+        env: { PORT: String(port) },
+      });
+      await verifyApp(port);
+      await kill(app2);
 
       // Teardown
-      console.info('cleaning..');
+      info('Cleaning..');
       await cleanDir(userDir);
-      console.info('cleaned');
+      success('Cleaned');
     },
     timeout * 4,
   );
