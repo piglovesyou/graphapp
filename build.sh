1e030a006e1abe7681022242f2fe38436a7e6335
piglovesyou <thepiglovesyou@gmail.com>
2019-07-20 13:12:24 +0900

    fix: care rimraf as deps

--- build.sh
+++ build.sh
@@ -5,12 +5,12 @@ set -e
 # Use fresh yarn
 npm i -g npm
 npm i -g yarn
+yarn install
 
 # Yarn holds old uwd so it doesn't use new packed one
 yarn cache clean uwf
 yarn run rimraf `yarn cache dir`/.tmp
 
-yarn install
 yarn lerna bootstrap
 yarn lerna run prepack
 yarn lerna run codegen
