f8f328ac5e860e409cb2ef679738ce91b7020fa2
piglovesyou <thepiglovesyou@gmail.com>
2019-07-26 09:58:36 +0900

    wip: pass routeContext

--- examples/basic/routes/posts/_id.tsx
+++ examples/basic/routes/posts/_id.tsx
@@ -21,7 +21,7 @@ const Home = withNews<Props>({
     if (!params || !params.id) throw new Error('never');
     return {
       variables: {
-        link: params.id,
+        link: Array.isArray(params.id) ? params.id[0] : params.id,
       },
     };
   },
--- packages/uwf/src/app/RouteContext.ts
+++ packages/uwf/src/app/RouteContext.ts
@@ -1,10 +1,11 @@
 import { createContext } from 'react';
 import { ParsedQuery } from 'query-string';
+import { QueryParams } from 'universal-router';
 
 export type RouteContextTypes = {
   pathname: string;
   query?: ParsedQuery<string>;
-  params?: { [s: string]: string };
+  params?: QueryParams;
 };
 
 const RouteContext = createContext<RouteContextTypes>({
