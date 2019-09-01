5fab43ddf3453de66b8da79f29df66b194651274
piglovesyou <thepiglovesyou@gmail.com>
2019-09-01 16:37:51 +0900

    include readme.md

--- packages/snapp/prepack.sh
+++ packages/snapp/prepack.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
-yarn run rimraf dist
-yarn run copyfiles -u 1 src/**/*.css dist
-yarn run copyfiles -u 4 '../../examples/basic/{components,data,public,routes,state,typings}/**' dist/examples/basic
-yarn run copyfiles -u 4 '../../examples/basic/{package.json,.gitignore,tsconfig.json}' dist/examples/basic
-yarn run copyfiles -u 1 'src/{app,config}/**' dist
-yarn run babel --config-file ../../babel.config --extensions '.ts,.tsx,.js' -d dist/tools src/tools
+set -e
+
+cp ../../README.md .
+
+npx rimraf dist
+npx copyfiles -u 1 src/**/*.css dist
+npx copyfiles -u 4 '../../examples/basic/{components,data,public,routes,state,typings}/**' dist/examples/basic
+npx copyfiles -u 4 '../../examples/basic/{package.json,.gitignore,tsconfig.json}' dist/examples/basic
+npx copyfiles -u 1 'src/{app,config}/**' dist
+npx babel --config-file ../../babel.config --extensions '.ts,.tsx,.js' -d dist/tools src/tools
+
+echo "prepack has been done."
